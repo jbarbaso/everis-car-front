d952de91c385fd35e5b2631b3ae92350cf020463
jbarbaso <jbarbaso@everis.com>
2019-04-17 15:30:46 +0200

    Changed mock-cars to uppercase

--- src/mock-cars.ts
+++ src/mock-cars.ts
@@ -1,10 +1,40 @@
-import {Car} from './app/cars/entities/car';
+import { Car } from './app/cars/entities/car';
 
-export const Cars: Car[] = [
-    {id: 13, brand: 'BMW', registration: new Date().toString(), country: 'Spain' },
-    {id: 14, brand: 'BMW', registration: new Date().toString(), country: 'Germany' },
-    {id: 18, brand: 'BMW', registration: new Date().toString(), country: 'England' },
-    {id: 19, brand: 'BMW', registration: new Date().toString(), country: 'Spain' },
-    {id: 20, brand: 'BMW', registration: new Date().toString(), country: 'Germany' },
-    {id: 23, brand: 'BMW', registration: new Date().toString(), country: 'England' }
+export const CARS: Car[] = [
+  {
+    id: 13,
+    brand: 'BMW',
+    registration: new Date().toString(),
+    country: 'Spain'
+  },
+  {
+    id: 14,
+    brand: 'BMW',
+    registration: new Date().toString(),
+    country: 'Germany'
+  },
+  {
+    id: 18,
+    brand: 'BMW',
+    registration: new Date().toString(),
+    country: 'England'
+  },
+  {
+    id: 19,
+    brand: 'BMW',
+    registration: new Date().toString(),
+    country: 'Spain'
+  },
+  {
+    id: 20,
+    brand: 'BMW',
+    registration: new Date().toString(),
+    country: 'Germany'
+  },
+  {
+    id: 23,
+    brand: 'BMW',
+    registration: new Date().toString(),
+    country: 'England'
+  }
 ];
